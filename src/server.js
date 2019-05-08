faae91790b48edd6461631d2cda02e80c880c02a
Ari <ari.abramowitz1@gmail.com>
2019-05-08 17:19:48 -0400

    working except for unique email message

--- src/server.js
+++ src/server.js
@@ -8,12 +8,12 @@ const server = http.createServer(app);
 
 server.listen(port);
 
-function normalizePort(val){
+function normalizePort(val) {
   const port = parseInt(val, 10);
-  if(isNaN(port)){
+  if (isNaN(port)) {
     return val;
   }
-  if(port >= 0){
+  if (port >= 0) {
     return port;
   }
   return false;
